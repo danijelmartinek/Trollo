bc9caa57ceba0bd07a4ecc99b412e93330d5d59e
Danijel Martinek <danijel.martinek.4@gmail.com>
2018-09-12 00:51:18 +0200

    added layout

---
+++ FrontEnd/src/App.vue
@@ -0,0 +1,40 @@
+<template>
+  <v-app
+    id="inspire"
+    dark
+  >
+    <Header v-on:toggleDrawer="toggleDrawer()"/>
+
+    <Sidebar v-bind:drawer="drawer"/>
+
+    <v-content>
+      <v-container fill-height>
+        <v-layout justify-center align-center>
+          
+        </v-layout>
+      </v-container>
+    </v-content>
+  </v-app>
+</template>
+
+<script>
+  import Header from './components/layout/Header.vue'
+  import Sidebar from './components/layout/Sidebar.vue'
+
+  export default {
+    data: () => ({
+      drawer: true
+    }),
+    methods: {
+      toggleDrawer: function () {
+        this.drawer = !this.drawer
+      }
+    },
+    
+    name: 'App',
+    components: {
+      Header,
+      Sidebar
+    }
+  }
+</script>
---
+++ FrontEnd/src/components/layout/Header.vue
@@ -0,0 +1,34 @@
+<template>
+  <v-toolbar
+    color="red"
+    dense
+    fixed
+    clipped-left
+    app
+  >
+    <v-toolbar-side-icon @click.stop="toggleDrawer()"></v-toolbar-side-icon>
+    <v-icon class="mx-1">fa dashboard</v-icon>
+    <v-toolbar-title class="mr-5 align-center">
+      <span class="title">Trollo</span>
+    </v-toolbar-title>
+  </v-toolbar>
+</template>
+
+<script>
+  export default {
+    data: () => ({
+      drawer: false
+    }),
+    methods: {
+      toggleDrawer: function () {
+        this.$emit("toggleDrawer")
+      }
+    },
+
+    name: 'Header',
+  }
+</script>
+
+<style scoped>
+
+</style>
